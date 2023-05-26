39369d1f05a660a8f57a1a03fb5a3b060398aa7b
sehoon <sehoon0519@naver.com>
2023-05-27 02:57:23 +0900

    support drawing with both hands

--- GestureRecognizer.py
+++ GestureRecognizer.py
@@ -2,7 +2,7 @@ import mediapipe as mp
 import cv2
 
 class GestureRecognizer:
-    def __init__(self, enable_tracking=False, max_hands=1, detection_confidence=0.5, tracking_confidence=0.8):
+    def __init__(self, enable_tracking=False, max_hands=2, detection_confidence=0.5, tracking_confidence=0.5):
         self.enable_tracking = enable_tracking
         self.max_hands = max_hands
         self.detection_confidence = detection_confidence
@@ -13,6 +13,7 @@ class GestureRecognizer:
                                          self.tracking_confidence)
         self.mp_draw = mp.solutions.drawing_utils
         self.results = None
+        self.landmarks = {'Right': None, 'Left': None}
 
     def detect_hands(self, image, draw=True):
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -23,36 +24,38 @@ class GestureRecognizer:
                 self.mp_draw.draw_landmarks(image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
         return image
 
-    def get_hand_position(self, image, hand_index=0, draw=True):
-        landmark_list = []
+    def get_hand_position(self, image, draw=True):
+        landmark_list = {'Right': [], 'Left': []}
         if self.results.multi_hand_landmarks:
-            my_hand = self.results.multi_hand_landmarks[hand_index]
-            for landmark in my_hand.landmark:
-                height, width, _ = image.shape
-                x, y = int(landmark.x * width), int(landmark.y * height)
-                landmark_list.append((x, y))
-
-                if draw:
-                    cv2.circle(image, (x, y), 5, (255, 0, 255), cv2.FILLED)
+            for hand_num, hand_landmarks in enumerate(self.results.multi_hand_landmarks):
+                hand_info = self.results.multi_handedness[hand_num]
+                hand_type = 'Right' if hand_info.classification[0].label == 'Right' else 'Left'
+                for landmark in hand_landmarks.landmark:
+                    height, width, _ = image.shape
+                    x, y = int(landmark.x * width), int(landmark.y * height)
+                    landmark_list[hand_type].append((x, y))
+                    if draw:
+                        cv2.circle(image, (x, y), 5, (255, 0, 255), cv2.FILLED)
         return landmark_list
 
     def count_up_fingers(self, image):
         # Get the hand positions from the image
         positions = self.get_hand_position(image, draw=False)
 
-        # Check if positions are available
-        if positions:
-            # Check if each finger is up and store the results
-            is_index_finger_up = positions[4][1] < positions[3][1] and (positions[5][0] - positions[4][0] > 10)
-            is_middle_finger_up = positions[8][1] < positions[7][1] and positions[7][1] < positions[6][1]
-            is_ring_finger_up = positions[12][1] < positions[11][1] and positions[11][1] < positions[10][1]
-            is_pinky_finger_up = positions[16][1] < positions[15][1] and positions[15][1] < positions[14][1]
-            is_thumb_finger_up = positions[20][1] < positions[19][1] and positions[19][1] < positions[18][1]
+        up_fingers = {'Right': [], 'Left': []}
 
-            # Store the results in a list
-            up_fingers = [is_index_finger_up, is_middle_finger_up, is_ring_finger_up, is_pinky_finger_up,
-                          is_thumb_finger_up]
-        else:
-            up_fingers = []
+        for hand in ['Right', 'Left']:
+            # Check if positions are available
+            if positions[hand]:
+                # Check if each finger is up and store the results
+                is_index_finger_up = positions[hand][4][1] < positions[hand][3][1] and (positions[hand][5][0] - positions[hand][4][0] > 10)
+                is_middle_finger_up = positions[hand][8][1] < positions[hand][7][1] and positions[hand][7][1] < positions[hand][6][1]
+                is_ring_finger_up = positions[hand][12][1] < positions[hand][11][1] and positions[hand][11][1] < positions[hand][10][1]
+                is_pinky_finger_up = positions[hand][16][1] < positions[hand][15][1] and positions[hand][15][1] < positions[hand][14][1]
+                is_thumb_finger_up = positions[hand][20][1] < positions[hand][19][1] and positions[hand][19][1] < positions[hand][18][1]
+
+                # Store the results in a list
+                up_fingers[hand] = [is_index_finger_up, is_middle_finger_up, is_ring_finger_up, is_pinky_finger_up,
+                                    is_thumb_finger_up]
 
         return up_fingers
--- old_main.py
+++ old_main.py
@@ -4,23 +4,28 @@ from Box import Box
 from GestureRecognizer import GestureRecognizer
 from FaceLandmarkDetector import FaceLandmarkDetector
 
-
 def run_whiteboard():
+    # Initialize the gesture recognizer
     recognizer = GestureRecognizer(detection_confidence=0.8)
     face_detector = FaceLandmarkDetector()
 
+    # Initialize the video capture object
     cap = cv2.VideoCapture(1)
     cap.set(3, 1280)
     cap.set(4, 720)
 
+    # create a canvas
     canvas = np.zeros((720, 1280, 3), np.uint8)
 
-    previous_points = {'Right': [0, 0], 'Left': [0, 0]}
-    color = {'Right': (255, 0, 0), 'Left': (255, 0, 0)}
+    # Previous x and y points
+    px, py = 0, 0
+
+    # Default drawing color
+    color = (255, 0, 0)
     brush_size = 5
     eraser_size = 40
-    last_hand_used = None
 
+    # Color data
     color_data = [
         (0, 0, 100, 100, (0, 0, 255)),  # Red
         (0, 100, 100, 100, (255, 0, 0)),  # Blue
@@ -29,8 +34,10 @@ def run_whiteboard():
         (0, 400, 100, 100, (0, 0, 0), "Eraser")  # Erase (black)
     ]
 
+    # Create color boxes
     colors = [Box(*data) for data in color_data]
 
+    # Create clear box
     clear = Box(0, 620, 100, 100, (100, 100, 100), "Clear")
 
     while True:
@@ -44,66 +51,70 @@ def run_whiteboard():
         positions = recognizer.get_hand_position(frame)
         up_fingers = recognizer.count_up_fingers(frame)
 
-        for hand in ['Right', 'Left']:
-            if positions[hand] and up_fingers[hand] and len(positions[hand]) > 8:
-                x, y = positions[hand][8]
-                last_hand_used = hand
+        # initialize x, y position
+        x, y = 0, 0
 
-                if up_fingers[hand][1] and up_fingers[hand][2]:
-                    previous_points[hand] = [0, 0]
+        if up_fingers:
+            x, y = positions[8][0], positions[8][1]
+            if up_fingers[1] and up_fingers[2]:
+                px, py = 0, 0
 
-                    for cb in colors:
-                        if cb.is_over(x, y):
-                            color[hand] = cb.color
+                # Select color
+                for cb in colors:
+                    if cb.is_over(x, y):
+                        color = cb.color
 
-                    if clear.is_over(x, y):
-                        canvas = np.zeros((720, 1280, 3), np.uint8)
+                # clear the canvas
+                if clear.is_over(x, y):
+                    canvas = np.zeros((720, 1280, 3), np.uint8)
 
-                elif up_fingers[hand][1] and not up_fingers[hand][2]:
-                    if previous_points[hand][0] != 0 and previous_points[hand][1] != 0:
-                        cv2.line(canvas, (previous_points[hand][0], previous_points[hand][1]), (x, y), color[hand], brush_size)
-                    previous_points[hand] = [x, y]
+            # Draw with index finger
+            elif up_fingers[1] and not up_fingers[2]:
+                if px != 0 and py != 0:
+                    cv2.line(canvas, (px, py), (x, y), color, brush_size)
+                px, py = x, y
 
-                else:
-                    previous_points[hand] = [0, 0]
-
-        if last_hand_used is not None and len(positions[last_hand_used]) > 8:
-            x, y = positions[last_hand_used][8]
-            pen_size_bar_x = 1180
-            pen_size_bar_y = 0
-            pen_size_bar_width = 100
-            pen_size_bar_height = 600
-            # pen_size_bar_color = color
-
-            if x >= pen_size_bar_x and x <= pen_size_bar_x + pen_size_bar_width:
-                progress = (y - pen_size_bar_y) / pen_size_bar_height
-                brush_size = int(progress * 20) + 5
-
-            for c in colors:
-                c.draw_rect(frame)
-                cv2.rectangle(frame, (c.x, c.y), (c.x + c.w, c.y + c.h), (0, 0, 0), 2)
-
-            clear.draw_rect(frame)
-            cv2.rectangle(frame, (clear.x, clear.y), (clear.x + clear.w, clear.y + clear.h), (0, 0, 0), 2)
-
-            # cv2.rectangle(frame, (pen_size_bar_x, pen_size_bar_y), (pen_size_bar_x + pen_size_bar_width, pen_size_bar_y + pen_size_bar_height), (0,0,0), -1)
-            # cv2.rectangle(frame, (pen_size_bar_x, pen_size_bar_y), (pen_size_bar_x + pen_size_bar_width, int(pen_size_bar_y + pen_size_bar_height * brush_size / 25)), pen_size_bar_color, -1)
-            if isinstance(color[last_hand_used], tuple) and len(color[last_hand_used]) == 3:  # Check if color is a valid BGR tuple
-                pen_size_bar_color = color[last_hand_used]
-                cv2.rectangle(frame, (pen_size_bar_x, pen_size_bar_y), (pen_size_bar_x + pen_size_bar_width, int(pen_size_bar_y + pen_size_bar_height * brush_size / 25)), pen_size_bar_color, -1)
             else:
-                print(f"Invalid color value: {color[last_hand_used]}")
+                px, py = 0, 0
 
-            pen_size_text = f"Pen Size: {brush_size}"
-            cv2.putText(frame, pen_size_text, (pen_size_bar_x, pen_size_bar_y + pen_size_bar_height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        # clear the canvas when middle finger is up
+        if up_fingers and up_fingers[2] and not up_fingers[1] and positions:
+            canvas = np.zeros((720, 1280, 3), np.uint8)
 
+        # merge the canvas and the frame
         canvas_gray = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
         _, img_inv = cv2.threshold(canvas_gray, 20, 255, cv2.THRESH_BINARY_INV)
         img_inv = cv2.cvtColor(img_inv, cv2.COLOR_GRAY2BGR)
         frame = cv2.bitwise_and(frame, img_inv)
         frame = cv2.bitwise_or(frame, canvas)
 
+        # Draw the color boxes
+        for c in colors:
+            c.draw_rect(frame)
+            cv2.rectangle(frame, (c.x, c.y), (c.x + c.w, c.y + c.h), (0, 0, 0), 2)
 
+        clear.draw_rect(frame)
+        cv2.rectangle(frame, (clear.x, clear.y), (clear.x + clear.w, clear.y + clear.h), (0, 0, 0), 2)
+
+        # Draw the pen size bar
+        pen_size_bar_x = 1180
+        pen_size_bar_y = 0
+        pen_size_bar_width = 100
+        pen_size_bar_height = 600
+        pen_size_bar_color = color  # Use the selected pen color
+
+        # Calculate the current pen size based on finger position
+        if x >= pen_size_bar_x and x <= pen_size_bar_x + pen_size_bar_width:
+            progress = (y - pen_size_bar_y) / pen_size_bar_height
+            brush_size = int(progress * 20) + 5  # Adjust the pen size range as needed
+
+        # Draw pen size progress bar
+        cv2.rectangle(frame, (pen_size_bar_x, pen_size_bar_y), (pen_size_bar_x + pen_size_bar_width, pen_size_bar_y + pen_size_bar_height), (0,0,0), -1)
+        cv2.rectangle(frame, (pen_size_bar_x, pen_size_bar_y), (pen_size_bar_x + pen_size_bar_width, int(pen_size_bar_y + pen_size_bar_height * brush_size / 25)), pen_size_bar_color, -1)  # Highlight the selected pen size based on brush_size
+
+        # Display current pen size value
+        pen_size_text = f"Pen Size: {brush_size}"
+        cv2.putText(frame, pen_size_text, (pen_size_bar_x, pen_size_bar_y + pen_size_bar_height + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         # Detect landmarks on the face
         landmarks = face_detector.detect_faces(frame, draw=True)
@@ -135,7 +146,6 @@ def run_whiteboard():
             else:
                 cv2.putText(frame, "Right Eye Closed", (550, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 cv2.rectangle(frame, (int(landmark.x * 1280) + eye_offset + 50, int(landmark.y * 720) - 50 - eye_offset + 10), (int(landmark.x * 1280) + eye_offset - 40, int(landmark.y * 720) - 50 - eye_offset - 10), (0, 0, 0), -1)
-
 
         cv2.imshow('video', frame)
         k = cv2.waitKey(1)
